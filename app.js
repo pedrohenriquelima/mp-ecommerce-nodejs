886bc2081cc8c0e62e5449c07ee1ca12d6ee8c7b
Pedro Lima <pedro.pedsilva@mercadolivre.com>
2021-03-17 11:14:31 -0300

    added process_payment test to app

--- app.js
+++ app.js
@@ -23,4 +23,36 @@ app.get('/checkout', function (req, res) {
     res.render('checkout', req.query);
 });
 
+app.post('/process_payment', function (req, res) {
+    var mercadopago = require('mercadopago');
+    mercadopago.configurations.setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+
+    var payment_data = {
+    transaction_amount: Number(req.body.transactionAmount),
+    token: req.body.token,
+    description: req.body.description,
+    installments: Number(req.body.installments),
+    payment_method_id: req.body.paymentMethodId,
+    issuer_id: req.body.issuer,
+    payer: {
+        email: req.body.email,
+        identification: {
+        type: req.body.docType,
+        number: req.body.docNumber
+        }
+    }
+    };
+
+    mercadopago.payment.save(payment_data)
+    .then(function(response) {
+        res.status(response.status).json({
+        status: response.body.status,
+        status_detail: response.body.status_detail,
+        id: response.body.id
+        });
+    })
+    .catch(function(error) {
+        res.status(response.status).send(error);
+    });
+})
 app.listen(port);
