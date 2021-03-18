2032bacbd4179e07004bcda070fc7ee62ace520f
Pedro Lima <pedro.pedsilva@mercadolivre.com>
2021-03-18 01:38:38 -0300

    added failure pending and success pages

--- app.js
+++ app.js
@@ -26,33 +26,54 @@ app.get('/detail', function (req, res) {
     let preference = {
         items: [
             {
+                id: 1234,
                 title: req.query.title,
+                description: 'Dispositivo móvil de Tienda e-commerce',
                 picture_url: req.headers.host + req.query.img.substring(1),
                 unit_price: parseFloat(req.query.price),
                 quantity: parseInt(req.query.unit),
             }
         ],
-        back_urls:{
-            success: req.headers.host + "/success",
-            pending: req.headers.host + "/pending",
-            failure: req.headers.host + "/failure"
+        payer: {
+            name: 'Lalo',
+            surname: 'Landa',
+            identification: {
+                type: 'DNI',
+                number: '22333444'
+            },
+            email: 'test_user_63274575@testuser.com',
+            phone: {
+                area_code: '011',
+                number: 22223333
+            },
+            address: {
+                street_name: 'Falsa',
+                street_number: 123,
+                zip_code: '1111'
+            }
         },
-        auto_return: "approved",
+        external_reference: 'pedrolimaesilva95@gmail.com',
+        back_urls: {
+            success: req.headers.host + '/success',
+            pending: req.headers.host + '/pending',
+            failure: req.headers.host + '/failure'
+        },
+        auto_return: 'approved',
         payment_methods: {
             excluded_payment_methods: [
                 {
-                    id: "amex"
+                    id: 'amex'
                 }
             ],
             excluded_payment_types: [
                 {
-                    id: "atm"
+                    id: 'atm'
                 }
             ],
             installments: 6
         },
     };
-    
+
     mercadopago.preferences.create(preference)
         .then(function (response) {
             req.query.preference_id = response.body.id;
@@ -61,11 +82,19 @@ app.get('/detail', function (req, res) {
             console.log(error);
             res.render('failure', req.query);
         });
-    
+
 });
 
 app.get('/checkout', function (req, res) {
     res.render('checkout', req.query);
+});
+
+app.get('/success', function (req, res) {
+    res.render('success', req.query);
+});
+
+app.get('/pending', function (req, res) {
+    res.render('pending', req.query);
 });
 
 app.get('/failure', function (req, res) {
