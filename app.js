8652143d7b8c3c1f5967d343abd8a248fda924c6
Pedro Lima <pedro.pedsilva@mercadolivre.com>
2021-03-17 22:59:11 -0300

    fixed global id from checkout button

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/detail', function (req, res) {
         access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
         integrator_id: 'dev_24c65fb163bf11ea96500242ac130004'
     });
-    console.log(req)
+
     let preference = {
         items: [
             {
@@ -31,22 +31,32 @@ app.get('/detail', function (req, res) {
                 unit_price: parseFloat(req.query.price),
                 quantity: parseInt(req.query.unit),
             }
-        ]
+        ],
+        back_urls:{
+            success: req.headers.host + "/success",
+            pending: req.headers.host + "/pending",
+            failure: req.headers.host + "/failure"
+        },
+        auto_return: "approved"
     };
     
     mercadopago.preferences.create(preference)
         .then(function (response) {
-            console.log(response)
-            global.id = response.body.id;
+            req.query.preference_id = response.body.id;
+            res.render('detail', req.query);
         }).catch(function (error) {
             console.log(error);
+            res.render('failure', req.query);
         });
-
-    res.render('detail', req.query);
+    
 });
 
 app.get('/checkout', function (req, res) {
     res.render('checkout', req.query);
+});
+
+app.get('/failure', function (req, res) {
+    res.render('failure', req.query);
 });
 
 app.listen(port);
